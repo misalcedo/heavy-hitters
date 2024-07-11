70fce068d1e8ab1f94dc60196aa7c3612c5e2e2d
Miguel D. Salcedo <miguel@salcedo.cc>
2024-07-11 19:12:16 -0400

    Ensure stream summary implements the heavy hitters interface.

--- main_test.go
+++ main_test.go
@@ -6,62 +6,65 @@ import (
 )
 
 func TestSpaceSaving(t *testing.T) {
+	var hh HeavyHitters[int]
+
 	stream := []int{12, 199997, 30000, 3, 8, 5, 10, 9, 2, 3, 5}
-	ss := NewStreamSummary[int](8)
+	hh = NewStreamSummary[int](8)
+
 	hits := 0
 
 	for _, e := range stream {
 		hits++
-		ss.Hit(e)
+		hh.Hit(e)
 	}
 
-	count := ss.Hit(5)
+	count := hh.Hit(5)
 	require.Equal(t, 3, count.Count)
 	require.Equal(t, 0, count.Error)
 
-	top, guaranteed := ss.Top(2)
+	top, guaranteed := hh.Top(2)
 	require.False(t, guaranteed)
 	require.Equal(t, []int{5, 2}, top)
 
-	frequent, guaranteed := ss.Frequent(0.1)
+	frequent, guaranteed := hh.Frequent(0.1)
 	require.True(t, guaranteed)
 	require.Equal(t, []int{5}, frequent)
 
-	require.Equal(t, hits+1, ss.Hits())
+	require.Equal(t, hits+1, hh.Hits())
 
-	count, found := ss.Get(12)
+	count, found := hh.Get(12)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 1, Error: 0}, count)
 
-	count, found = ss.Get(199997)
+	count, found = hh.Get(199997)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 1, Error: 0}, count)
 
-	count, found = ss.Get(30000)
+	count, found = hh.Get(30000)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 1, Error: 0}, count)
 
-	count, found = ss.Get(3)
+	count, found = hh.Get(3)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 2, Error: 0}, count)
 
-	count, found = ss.Get(8)
+	count, found = hh.Get(8)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 1, Error: 0}, count)
 
-	count, found = ss.Get(5)
+	count, found = hh.Get(5)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 3, Error: 0}, count)
 
-	count, found = ss.Get(10)
+	count, found = hh.Get(10)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 1, Error: 0}, count)
 
-	count, found = ss.Get(9)
+	count, found = hh.Get(9)
 	require.False(t, found)
 	require.Equal(t, Count{Count: 0, Error: 0}, count)
 
-	count, found = ss.Get(2)
+	count, found = hh.Get(2)
 	require.True(t, found)
 	require.Equal(t, Count{Count: 2, Error: 1}, count)
 }
